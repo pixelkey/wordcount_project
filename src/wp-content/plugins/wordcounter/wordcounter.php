93a20f4eb920bfac3fac28b57353c687d45295e8
pixelkey-support <support@pixelkey.com>
2024-11-11 09:15:50 +1030

    Wordcount with Animated counter

--- src/wp-content/plugins/wordcounter/wordcounter.php
+++ src/wp-content/plugins/wordcounter/wordcounter.php
@@ -12,43 +12,43 @@ Version: 0.0.1
 Author URI: http://google.com
 */
 
-function examplefn()
-{
-	/** These are the lyrics to Hello Dolly */
-	$lyrics = "Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-I feel the room swayin'
-While the band's playin'
-One of our old favorite songs from way back when
-So, take her wrap, fellas
-Dolly, never go away again
-Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-I feel the room swayin'
-While the band's playin'
-One of our old favorite songs from way back when
-So, golly, gee, fellas
-Have a little faith in me, fellas
-Dolly, never go away
-Promise, you'll never go away
-Dolly'll never go away again";
+// function examplefn()
+// {
+// 	/** These are the lyrics to Hello Dolly */
+// 	$lyrics = "Hello, Dolly
+// Well, hello, Dolly
+// It's so nice to have you back where you belong
+// You're lookin' swell, Dolly
+// I can tell, Dolly
+// You're still glowin', you're still crowin'
+// You're still goin' strong
+// I feel the room swayin'
+// While the band's playin'
+// One of our old favorite songs from way back when
+// So, take her wrap, fellas
+// Dolly, never go away again
+// Hello, Dolly
+// Well, hello, Dolly
+// It's so nice to have you back where you belong
+// You're lookin' swell, Dolly
+// I can tell, Dolly
+// You're still glowin', you're still crowin'
+// You're still goin' strong
+// I feel the room swayin'
+// While the band's playin'
+// One of our old favorite songs from way back when
+// So, golly, gee, fellas
+// Have a little faith in me, fellas
+// Dolly, never go away
+// Promise, you'll never go away
+// Dolly'll never go away again";
 
-	// Here we split it into lines.
-	$lyrics = explode("\n", $lyrics);
+// 	// Here we split it into lines.
+// 	$lyrics = explode("\n", $lyrics);
 
-	// And then randomly choose a line.
-	return wptexturize($lyrics[mt_rand(0, count($lyrics) - 1)]);
-}
+// 	// And then randomly choose a line.
+// 	return wptexturize($lyrics[mt_rand(0, count($lyrics) - 1)]);
+// }
 
 
 function word_count()
@@ -60,24 +60,124 @@ function word_count()
 	return $word_count;
 }
 
-function display_header()
+// function enter_data($atts, $data = null)
+// {
+// 	// $data = $_GET("Enter the data");
+// 	if (is_null($data)) {
+// 		return "<p>Please enter the data</p>";
+// 	}
+
+// 	$data = sanitize_text_field($data);
+// $words = str_word_count($data);
+// $ans = "<p>Total word count is: $words</p>";
+// return $data;
+// }
+
+function display_header($atts)
 {
-	$count = word_count();
-	printf('<h1 class = "wcount">Total Word count is: %d</h1>', $count);
+	$Art = get_the_content();
+	$Art = strip_tags($Art);
+	$count = str_word_count($Art);
+
+	$counter = '
+	<div class="wcount">Article word count is: <br><span id="counter" class= "wnumber">0</span></div>
+    <script type="text/javascript">
+        var counterElement = document.getElementById("counter");
+        var counter = 0;
+        var maxCount = ' . $count . ';
+        var interval = 100;
+
+        var timer = setInterval(function() {
+            counterElement.textContent = counter;
+            counter++;
+            if (counter > maxCount) {
+                clearInterval(timer);
+            }
+        }, interval);
+    </script>
+    ';
+
+	return $counter;
+
+	//return "<div class='wcount'>Article's Word count is: <div class='wnumber' span id='num'>$count</span></div></div>";
 }
+
 
 function count_style()
 {
 	echo "
 	<style type='text/css'>
 		.wcount {
-			color: yellow;
-			background-color: Black;
+			font-family: Georgia;
+			font-size: 30px;
 			text-align: center;
-			font-size: 16px;
+			border-style: out set;
+			background-color: #f1f1f1;
+			padding-top: 10px;
+			margin-bottom: 0px;
+			
 		}
 	</style>";
 }
 
+function number_style()
+{
+	echo "
+	<style type='text/css'>
+		.wnumber {
+			font-family: Copperplate,fantasy;
+			font-size: 70px;
+			text-align: center;
+			border-style: outset;
+			background-color: #f1f1f1;
+			padding-bottom: 10px;
+		}
+	</style>";
+}
+
+
+add_action('wp_head', 'number_style');
 add_action('wp_head', 'count_style');
-add_action('wp_body_open', 'display_header', -1);
+add_shortcode('word_counter', 'display_header');
+
+// add_action('wp_body_open', 'display_header', -1);
+// add_shortcode('wordcounter', 'display_header');
+// add_shortcode('data_entry', 'enter_data');
+// function enter_data($atts, $content = null)
+// {
+// 	// Check if content is provided
+// 	if (empty($content)) {
+// 		return "<p>Please enter the data inside the shortcode.</p>";
+// 	}
+
+// 	// Sanitize the content
+// 	$data = sanitize_text_field($content);
+
+// 	// Count the words
+// 	$words = str_word_count($data);
+
+// 	// Prepare the output
+// 	$ans = "<p class='wcount'>Total word count is: $words</p>";
+
+// 	return $ans;
+// }
+
+// function count_style()
+// {
+// 	echo "
+//     <style type='text/css'>
+//         .wcount {
+//             color: yellow;
+//             background-color: black;
+//             font-size: 30px;
+//             padding: 10px;
+//             text-align: center;
+//         }
+//     </style>";
+// }
+
+// // Hook the styles into wp_head
+// add_action('wp_head', 'count_style');
+
+// // Register the shortcode [word_counter]
+// add_shortcode('word_counter', 'enter_data');
