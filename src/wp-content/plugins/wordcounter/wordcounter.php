01421d23b8fc98b310139f59f2de752939c3c17a
pixelkey-support <support@pixelkey.com>
2024-11-26 15:39:25 +1030

    Implemented CSS Skin and Radio Button for Style in frontend

--- src/wp-content/plugins/wordcounter/wordcounter.php
+++ src/wp-content/plugins/wordcounter/wordcounter.php
@@ -12,6 +12,7 @@ Version: 0.0.1
 Author URI: http://google.com
 */
 
+/* Function to get the word count of the post */
 function word_count()
 {
 	$content = get_the_content();
@@ -21,16 +22,24 @@ function word_count()
 	return $word_count;
 }
 
-
+/* Function to display the word count in the front end with custom CSS as per the user*/
 
 function display_header($atts)
 {
 
-	$atts = shortcode_atts(array(
-		'style' => 'bluesky',
-	), $atts, 'word_counter');
+	$allowed_styles = array('bluesky', 'blackgrey', 'blackwhite', 'blackneon');
 
-	$style = $atts['style'];
+	if (isset($_POST['selected_style']) && in_array($_POST['selected_style'], $allowed_styles)) {
+		$style = $_POST['selected_style'];
+	} else {
+		$style = 'bluesky';
+	}
+
+	// $atts = shortcode_atts(array(
+	// 	'style' => 'bluesky',
+	// ), $atts, 'word_counter');
+
+	// $style = $atts['style'];
 
 	if ($style == 'bluesky') {
 		wp_enqueue_style('custom-counter-styles', plugin_dir_url(__FILE__) . 'public/css/bluesky.css');
@@ -38,6 +47,8 @@ function display_header($atts)
 		wp_enqueue_style('custom-counter-styles2', plugin_dir_url(__FILE__) . 'public/css/blackgrey.css');
 	} elseif ($style == 'blackwhite') {
 		wp_enqueue_style('custom-counter-styles3', plugin_dir_url(__FILE__) . 'public/css/blackwhite.css');
+	} elseif ($style == 'blackneon') {
+		wp_enqueue_style('custom-counter-styles4', plugin_dir_url(__FILE__) . 'public/css/blackneon.css');
 	} else {
 		wp_enqueue_style('custom-counter-styles', plugin_dir_url(__FILE__) . 'public/css/bluesky.css');
 	}
@@ -93,3 +104,47 @@ function display_header($atts)
 
 add_shortcode('word_counter', 'display_header');
 
+function wp_enqueue_styles9()
+{
+	wp_enqueue_style('button-styles', plugin_dir_url(__FILE__) . 'public/css/button.css');
+}
+
+add_action('wp_enqueue_scripts', 'wp_enqueue_styles9');
+
+function select_style()
+{
+	$allowed_styles = array('bluesky', 'blackgrey', 'blackwhite', 'blackneon');
+
+	if (isset($_POST['selected_style']) && in_array($_POST['selected_style'], $allowed_styles)) {
+		$current_style = $_POST['selected_style'];
+	} else {
+		$current_style = 'bluesky';
+	}
+
+	$output = '<form method="post" class="style-selector-form">';
+	$styles = array('bluesky', 'blackgrey', 'blackwhite', 'blackneon');
+	foreach ($styles as $style) {
+		$checked = ($current_style == $style) ? 'checked' : '';
+		$style_name = ucfirst($style); // Capitalize the first letter
+		$output .= '<div class="button"><label><input type="radio" name="selected_style" value="' . $style . '" ' . $checked . '> ' . $style_name . '</label><br></div>';
+	}
+
+
+	$output .= '</form>';
+	$output .= '
+    <script type="text/javascript">
+    document.addEventListener("DOMContentLoaded", function() {
+        var radios = document.querySelectorAll(".style-selector-form input[name=\'selected_style\']");
+        radios.forEach(function(radio) {
+            radio.addEventListener("change", function() {
+                this.form.submit();
+            });
+        });
+    });
+    </script>
+    ';
+
+	return $output;
+}
+
+add_shortcode('select_style', 'select_style');
